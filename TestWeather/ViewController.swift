1eb0ca74dd559483fd3388bb6fe1a182620da51f
Bao <bao@Baos-MacBook-Pro.local>
2019-03-21 11:19:22 +0700

    Initial Commit

---
+++ TestWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWeather
+//
+//  Created by Bao on 3/21/19.
+//  Copyright © 2019 Bao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
