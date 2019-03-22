9f9af5cd7a5c5b36ea878fd228df03809bd47c7f
Bao <bao@Baos-MacBook-Pro.local>
2019-03-22 11:17:31 +0700

    first commit

---
+++ TestWeather/Cell/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TestWeather
+//
+//  Created by Bao on 3/22/19.
+//  Copyright © 2019 Bao. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TestWeather/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  TestWeather
+//
+//  Created by Bao on 3/22/19.
+//  Copyright © 2019 Bao. All rights reserved.
+//
+
+import Foundation
